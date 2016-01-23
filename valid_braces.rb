657c621fc357b699a749427c103dae81328af114
BenjaGross <Benja.J.Gross@gmail.com>
2016-01-23 12:12:17 -0500

    adds support for smart quotes

--- valid_braces.rb
+++ valid_braces.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'pry'
 
 
 class Brackets 
@@ -6,36 +7,39 @@ class Brackets
   def self.test_brackets(input)
     stack = []
     brack = input.split("").flatten
-    result = true
-    bracket_hash = {"]" => "[", "}" => "{", ")" => "(", ">" => "<"}
+    bracket_hash = {"]" => "[", "}" => "{", ")" => "(", ">" => "<", "’" => "‘", '”' => '“'}
     opening_brackets = bracket_hash.values
-
-    brack.each do |bracket|
-      if opening_brackets.include?(bracket)
-        stack.push(bracket)
-      else 
+    result = true
+    i = 0
+    while (result == true)&& (i < brack.length)
+      if opening_brackets.include?(brack[i])
+        stack.push(brack[i])
+        i+=1
+      else
         b = stack.pop
-        result = b == bracket_hash[bracket]
+        result = b == bracket_hash[brack[i]]
+        i +=1
       end
     end
-    result
+    result && stack.length == 0
   end
 end
 
-
 describe 'Brackets' do 
   let( :valid_string) { "([](<{}>))" }
-  let( :invalid_string) {"({<)>}"}
+  let( :invalid_string) { "({<)>}" }
+  let( :valid_with_quotes) { "([‘’](<{}>))" }
+  let( :invalid_with_quotes) {"([‘](’<{}>))"}
 
   it "returns true with a valid brakcet string" do
     expect(Brackets.test_brackets(valid_string)).to be(true)
+    expect(Brackets.test_brackets(valid_with_quotes)).to be(true)
   end
 
   it "returns false with an invalid bracket string" do 
     expect(Brackets.test_brackets(invalid_string)).to be(false)
+    expect(Brackets.test_brackets(invalid_with_quotes)).to be(false)
   end
 
+
 end
-
-
-
