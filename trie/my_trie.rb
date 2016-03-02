73f22bd5d2cb182279f606d2b3f7190bd090363b
BenjaGross <Benja.J.Gross@gmail.com>
2016-03-02 15:48:20 -0500

    finishes my trie

--- trie/my_trie.rb
+++ trie/my_trie.rb
@@ -11,16 +11,16 @@ class Trie
 
    def insert(word)
       characters = word.split("")         
-      lookup = self.tree
-      characters[0..-2].each{ |char| lookup = lookup[char] }
-      lookup = lookup[characters.last][true]
+      pointer = self.tree
+      characters[0..-2].each{ |char| pointer = pointer[char] }
+      pointer = pointer[characters.last][true]
    end
 
    def match(word)
       characters = word.split("")           
-      lookup = self.tree
-      characters.each { |char| lookup = lookup.fetch(char, nil) rescue nil }
-      result = lookup.fetch(true, nil) rescue nil
+      pointer = self.tree
+      characters.each { |char| pointer = pointer.fetch(char, nil) rescue nil }
+      result = pointer.fetch(true, nil) rescue nil
       result == {} ? true : false 
    end
 end
