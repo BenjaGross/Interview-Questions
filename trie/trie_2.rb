1fb59d8c059ad03b5d82982190af13251d089606
BenjaGross <Benja.J.Gross@gmail.com>
2016-03-02 13:16:08 -0500

    finishes final trie with eval string

--- trie/trie_2.rb
+++ trie/trie_2.rb
@@ -10,31 +10,28 @@ class Trie
    end
 
    def insert(word)
-      characters = word.split(//u)  
-      word_length = characters.size          
-      insert_word_string = ""
-      word_length.times { |x| insert_word_string << "[characters.at(#{x})]" }
-      insert_word_string = "self.tree" << str << "[true]"
-      # str = "self.tree[characters.at(0)][characters.at(1)][characters.at(2)][characters.at(3)][true]"
-      eval(insert_word_string)
+      characters = word.split("")         
+      insert_string = ""
+      characters.each { |char| insert_string << "['#{char}']" }
+      insert_string = "self.tree" << insert_string << "[true]"
+      # insert_string = "self.tree[characters.at(0)][characters.at(1)][characters.at(2)][characters.at(3)][true]"
+      eval(insert_string)
    end
 
    def match(word)
-      characters = word.split(//u) 
-      word_length = characters.size           
-      str = ""
-      word_length.times { |x| str << ".fetch(characters.at(#{x}), nil)" }
-      str = "self.tree" << str << ".fetch(true, nil)"
-      # str = "self.tree.fetch(characters.at(0), nil).fetch(characters.at(1), nil).fetch(characters.at(2), nil).fetch(true, nil)"
-      ret = eval(str) rescue nil
-
-      ret == {} ? true : false 
+      characters = word.split("")           
+      match_string = ""
+      characters.each{ |char| match_string << ".fetch('#{char}', nil)" }
+      match_string = "self.tree" << match_string << ".fetch(true, nil)"
+      # match_string = "self.tree.fetch(characters.at(0), nil).fetch(characters.at(1), nil).fetch(characters.at(2), nil).fetch(true, nil)"
+      result = eval(match_string) rescue nil
+      result == {} ? true : false 
    end
 end
 
-# describe "Trie" do
-#    it "returns true" do
-#      collection.include?(7).should be_true
-#    end
+trie = Trie.new
+trie.insert("salad")
+trie.insert("sapple")
+puts trie.match("salad")
+puts trie.match("sapple")
 
-# end
