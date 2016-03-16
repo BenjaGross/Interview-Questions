c54d7d5869e0ea75984c73b05e0de649b5a4f739
BenjaGross <Benja.J.Gross@gmail.com>
2016-03-16 10:52:30 -0400

    runs tail optimized recursion

--- recursion/optimized_recursion.py
+++ recursion/optimized_recursion.py
@@ -1,2 +1,13 @@
 def runner():
-  print int(input("Number for factorial.\n"))
+  num = int(input("Number for factorial: "))
+  total = 1
+  fact = factorial(num, total)
+  print("Factorial is",fact)
+
+def factorial(num, total):
+  if num == 0:
+    return total
+  else: 
+    return factorial(num -1, total * num)
+
+runner()
